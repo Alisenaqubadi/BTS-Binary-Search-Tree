ef3f4100c82c64ac6b11da24ace3c2cc72f492d5
Alisenaqubadi <131773898+Alisenaqubadi@users.noreply.github.com>
2025-09-19 23:09:41 +0500

    Project was completed

--- src/BTS.js
+++ src/BTS.js
@@ -1,158 +1,270 @@
+import { Queue } from './queue.js'
+
 class Node {
-    constructor(value,left = null, right = null){
-        this.left = left
-        this.value = value
-        this.right = right
-    }
+  constructor(value, left = null, right = null) {
+    this.left = left
+    this.value = value
+    this.right = right
+  }
 }
 
 class BTS {
+  holeTree
 
-    holeTree;
+  buildTree(array, firstTime = true) {
+    let newArray
 
-    buildTree(array,firstTime = true){
-
-        let newArray;
-        
-        if(firstTime){
-            newArray = FilterTheDouble(array);
-        } else {
-            newArray = array;
-        }
-
-        if(newArray.length === 1){
-          return new Node(newArray[0]);
-        }
-
-        if(newArray.length === 0){
-             return null; 
-        }
-
-        const mid = Math.floor(newArray.length / 2);
-        
-        const root = newArray[mid];
-        const left = newArray.slice(0,mid);
-        const right = newArray.slice(mid + 1);
-
-        this.holeTree = new Node(root,this.buildTree(left,false),this.buildTree(right,false));
-      return this.holeTree;
-    }
-
-    insert(value){
-        let current = this.holeTree;
-        while (true) {
-    if (value < current.value) {
-      if (current.left === null) {
-        current.left = new Node(value);
-        return;
-      }
-      current = current.left;
+    if (firstTime) {
+      newArray = FilterTheDouble(array)
     } else {
-      if (current.right === null) {
-        current.right = new Node(value);
-        return;
-      }
-      current = current.right;
-    }}
+      newArray = array
     }
 
-    deleteItem(value) {
-    let current = this.holeTree;
-    let parent = null;
+    if (newArray.length === 1) {
+      return new Node(newArray[0])
+    }
+
+    if (newArray.length === 0) {
+      return null
+    }
+
+    const mid = Math.floor(newArray.length / 2)
+
+    const root = newArray[mid]
+    const left = newArray.slice(0, mid)
+    const right = newArray.slice(mid + 1)
+
+    this.holeTree = new Node(
+      root,
+      this.buildTree(left, false),
+      this.buildTree(right, false)
+    )
+    return this.holeTree
+  }
+
+  insert(value) {
+    let current = this.holeTree
+    while (true) {
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = new Node(value)
+          return
+        }
+        current = current.left
+      } else {
+        if (current.right === null) {
+          current.right = new Node(value)
+          return
+        }
+        current = current.right
+      }
+    }
+  }
+
+  deleteItem(value) {
+    let current = this.holeTree
+    let parent = null
 
     while (current !== null) {
-        if (value < current.value) {
-            parent = current;
-            current = current.left;
-        } else if (value > current.value) {
-            parent = current;
-            current = current.right;
+      if (value < current.value) {
+        parent = current
+        current = current.left
+      } else if (value > current.value) {
+        parent = current
+        current = current.right
+      } else {
+        if (current.left === null && current.right === null) {
+          if (parent === null) {
+            this.holeTree = null
+          } else if (parent.left === current) {
+            parent.left = null
+          } else {
+            parent.right = null
+          }
+        } else if (current.left === null || current.right === null) {
+          let child = current.left || current.right
+          if (parent === null) {
+            this.holeTree = child
+          } else if (parent.left === current) {
+            parent.left = child
+          } else {
+            parent.right = child
+          }
         } else {
+          let succParent = current
+          let succ = current.right
+          while (succ.left !== null) {
+            succParent = succ
+            succ = succ.left
+          }
+          current.value = succ.value
 
-            if (current.left === null && current.right === null) {
-                if (parent === null) {
-                    this.holeTree = null; 
-                } else if (parent.left === current) {
-                    parent.left = null;
-                } else {
-                    parent.right = null;
-                }
-            }
+          if (succParent.left === succ) {
+            succParent.left = succ.right
+          } else {
+            succParent.right = succ.right
+          }
+        }
+        return
+      }
+    }
+  }
 
-            else if (current.left === null || current.right === null) {
-                let child = current.left || current.right;
-                if (parent === null) {
-                    this.holeTree = child; 
-                } else if (parent.left === current) {
-                    parent.left = child;
-                } else {
-                    parent.right = child;
-                }
-            }
+  preOrderForEach(callback){
+    let current = this.holeTree;
+    callback(preOrderDigging(current))
+  }
 
-            else {
-                
-                let succParent = current;
-                let succ = current.right;
-                while (succ.left !== null) {
-                    succParent = succ;
-                    succ = succ.left;
-                }
-                current.value = succ.value;
-                
-                if (succParent.left === succ) {
-                    succParent.left = succ.right;
-                } else {
-                    succParent.right = succ.right;
-                }
-            }
-            return; 
+  InOrderForEach(callback){
+    let current = this.holeTree;
+    callback(InOrderDigging(current))
+  }
+
+  postOrderForEach(callback){
+    let current = this.holeTree;
+    callback(postOrderDigging(current))
+  }
+
+  levelOrderForEach(callback) {
+    let check = true
+    if (callback == undefined) {
+      console.error('The callback is undefined in levelOrderForEach function')
+      check = false
+    }
+
+    let queue = new Queue()
+    queue.enqueue(this.holeTree)
+    let array = []
+
+    while (!queue.isEmpty()) {
+      let current = queue.dequeue()
+      array.push(current.value)
+      if (current.left !== null) queue.enqueue(current.left)
+      if (current.right !== null) queue.enqueue(current.right)
+    }
+    if (check) callback(array)
+  }
+
+  height(value){
+    let current = this.holeTree
+
+    while(current.value !== value){
+        if (value < current.value) {
+            current = current.left;
+        } else if (current.value < value){
+            current = current.right;
         }
     }
+
+    return {value:current.value, height:digAndCountTheHeight(current)}
+  }
+
+  depth(value){
+    let current = this.holeTree
+    let Counter = 0;
+    while(current.value !== value){
+        if (value < current.value) {
+            current = current.left;
+            Counter++
+        } else if (current.value < value){
+            current = current.right;
+            Counter++
+        }
+    }
+
+    return {value:current.value, depth:Counter}
+  }
+
+  isBalanced(){
+    const current = this.holeTree
+    const left = digAndCountTheHeight(current.left)
+    const right = digAndCountTheHeight(current.right)
+    const def = left - right
+    if(def == 0) return true;
+    if(def == 1 ) return true;
+    if(def == -1) return true;
+    else return false;
+  }
+
+  rebalance(){
+    this.preOrderForEach((e)=>{this.buildTree(e)})
+  }
 }
 
+function preOrderDigging(current){
+   if (current === null) return [];     
+  let array = [];
+  array.push(current.value);          
+  array = array.concat(preOrderDigging(current.left));  
+  array = array.concat(preOrderDigging(current.right));  
+
+  return array;
+}
+
+function InOrderDigging(current){
+   if (current === null) return [];     
+  let array = [];
+  array = array.concat(InOrderDigging(current.left));  
+  array.push(current.value);          
+  array = array.concat(InOrderDigging(current.right));  
+
+  return array;
+}
+
+function postOrderDigging(current){
+   if (current === null) return [];     
+  let array = [];
+  array = array.concat(postOrderDigging(current.left));  
+  array = array.concat(postOrderDigging(current.right));  
+  array.push(current.value);          
+
+  return array;
+}
+
+function digAndCountTheHeight(current){
+     if (current === null) return -1; 
+  let left = digAndCountTheHeight(current.left);
+  let right = digAndCountTheHeight(current.right);
+  return 1 + Math.max(left, right);
 }
 
 function FilterTheDouble(array) {
-    const newArray = mergeSort(array);
-    let filter = [];
+  const newArray = mergeSort(array)
+  let filter = []
 
-    for (let i = 0; i < newArray.length; i++) {
-        if (filter.length === 0 || filter[filter.length - 1] !== newArray[i]) {
-            filter.push(newArray[i]);
-        }
+  for (let i = 0; i < newArray.length; i++) {
+    if (filter.length === 0 || filter[filter.length - 1] !== newArray[i]) {
+      filter.push(newArray[i])
     }
-    return filter;
+  }
+  return filter
 }
 
 function mergeSort(array) {
+  if (array.length <= 1) return array
 
-    if (array.length <= 1) return array;
+  const mid = Math.floor(array.length / 2)
+  const leftHalf = array.slice(0, mid)
+  const rightHalf = array.slice(mid)
 
+  const sortedLeft = mergeSort(leftHalf)
+  const sortedRight = mergeSort(rightHalf)
 
-    const mid = Math.floor(array.length / 2);
-    const leftHalf = array.slice(0, mid);
-    const rightHalf = array.slice(mid);
+  const merged = []
+  let i = 0,
+    j = 0
 
-
-    const sortedLeft = mergeSort(leftHalf);
-    const sortedRight = mergeSort(rightHalf);
-
-
-    const merged = [];
-    let i = 0, j = 0;
-
-    while (i < sortedLeft.length && j < sortedRight.length) {
-        if (sortedRight[j] < sortedLeft[i]) {
-            merged.push(sortedRight[j]);
-            j++;
-        } else {
-            merged.push(sortedLeft[i]);
-            i++;
-        }
+  while (i < sortedLeft.length && j < sortedRight.length) {
+    if (sortedRight[j] < sortedLeft[i]) {
+      merged.push(sortedRight[j])
+      j++
+    } else {
+      merged.push(sortedLeft[i])
+      i++
     }
+  }
 
-    return merged.concat(sortedLeft.slice(i)).concat(sortedRight.slice(j));
+  return merged.concat(sortedLeft.slice(i)).concat(sortedRight.slice(j))
 }
 
 export { BTS }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,56 @@
 import { BTS } from './BTS.js'
 
-const tree = new BTS();
+const tree = new BTS()
 tree.buildTree([
-  42, 17, 93, 58, 23, 7, 81, 36, 65, 14,
-  99, 1, 56, 27, 73, 88, 19, 4, 12, 31,
-  68, 25, 39, 52, 95, 3, 9, 18, 21, 29,
-  33, 47, 61, 70, 85, 90, 15, 6, 8, 11,
-  22, 26, 38, 44, 49, 53, 60, 72, 80, 100
-]);
+  42, 17, 93, 58, 23, 7, 81, 36, 65, 14, 99, 1, 56, 27, 73, 88, 19, 4, 12, 31,
+  68, 25, 39, 52, 95, 3, 9, 18, 21, 29, 33, 47, 61, 70, 85, 90, 15, 6, 8, 11,
+  22, 26, 38, 44, 49, 53, 60, 72, 80, 100,
+])
 
-function prettyPrint(node, prefix = "", isLeft = true) {
-    if (node === null) {
-        return;
-    }
-    if (node.right !== null) {
-        prettyPrint(node.right, prefix + (isLeft ? "│   " : "    "), false);
-    }
+function prettyPrint(node, prefix = '', isLeft = true) {
+  if (node === null) {
+    return
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, prefix + (isLeft ? '│   ' : '    '), false)
+  }
 
-    console.log(prefix + (isLeft ? "└── " : "┌── ") + node.value);
+  console.log(prefix + (isLeft ? '└── ' : '┌── ') + node.value)
 
-    if (node.left !== null) {
-        prettyPrint(node.left, prefix + (isLeft ? "    " : "│   "), true);
-    }
+  if (node.left !== null) {
+    prettyPrint(node.left, prefix + (isLeft ? '    ' : '│   '), true)
+  }
 }
 
-prettyPrint(tree.holeTree);
+prettyPrint(tree.holeTree)
 tree.deleteItem(39)
-prettyPrint(tree.holeTree);
+prettyPrint(tree.holeTree)
+tree.levelOrderForEach((e) => {
+  console.log(e)
+})
+
+console.log(tree.height(42))
+console.log(tree.depth(3))
+console.log(tree.depth(27))
+
+tree.preOrderForEach((e)=>{
+    console.log(e)
+})
+
+tree.InOrderForEach((e)=>{
+    console.log(e)
+})
+tree.postOrderForEach((e)=>{
+    console.log(e)
+})
+console.log(tree.isBalanced())
+tree.insert(-1)
+tree.insert(-2)
+tree.insert(-3)
+prettyPrint(tree.holeTree)
+console.log(tree.isBalanced())
+tree.rebalance()
+prettyPrint(tree.holeTree)
+console.log(tree.isBalanced())
+
 
